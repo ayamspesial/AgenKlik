0eed97c061712c6b541b6c9471c2275b36eee252
Russell <russellfebrisa@gmail.com>
2024-01-09 23:24:43 +0700

    Updates

---
+++ precache-manifest.23928c65352876962c6e565f3457562f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca10107912f35d713f998528ec1e56d",
+    "url": "/AgenKlik/index.html"
+  },
+  {
+    "revision": "8931e3d5173f98991ae3",
+    "url": "/AgenKlik/static/css/main.f85e24b6.chunk.css"
+  },
+  {
+    "revision": "5e81dec688bc4928fedc",
+    "url": "/AgenKlik/static/js/2.b4dcd3a1.chunk.js"
+  },
+  {
+    "revision": "f4e372463eca9aa1d6433868772ef3b7",
+    "url": "/AgenKlik/static/js/2.b4dcd3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca1266bbfb64412120e",
+    "url": "/AgenKlik/static/js/3.8bcdfb4d.chunk.js"
+  },
+  {
+    "revision": "8931e3d5173f98991ae3",
+    "url": "/AgenKlik/static/js/main.de9832fd.chunk.js"
+  },
+  {
+    "revision": "8af56ad14c777a1a697a",
+    "url": "/AgenKlik/static/js/runtime-main.b62f168d.js"
+  },
+  {
+    "revision": "60229fc800e1a3ea45978a15842204e7",
+    "url": "/AgenKlik/static/media/Blue Clipbaord.60229fc8.jpg"
+  },
+  {
+    "revision": "78a5137f5e588b9da7b20b3bafb5d5e0",
+    "url": "/AgenKlik/static/media/Circles.78a5137f.png"
+  },
+  {
+    "revision": "aa4e01670ff8f97b1f668760636abc7f",
+    "url": "/AgenKlik/static/media/Momcake-Thin.aa4e0167.otf"
+  },
+  {
+    "revision": "fae334733e655ce75f2d9776527d39ab",
+    "url": "/AgenKlik/static/media/Untitled.fae33473.png"
+  },
+  {
+    "revision": "e24c344e0f9a4af3f31a038ccf9c8612",
+    "url": "/AgenKlik/static/media/agriculture.e24c344e.png"
+  },
+  {
+    "revision": "c9814841b587107a621243ce130151bb",
+    "url": "/AgenKlik/static/media/baby-sitter.c9814841.png"
+  },
+  {
+    "revision": "0f4bf1364258902adf14bcb015238138",
+    "url": "/AgenKlik/static/media/caretaker.0f4bf136.png"
+  },
+  {
+    "revision": "5eceee1571edd60175e58d2a826dc0a2",
+    "url": "/AgenKlik/static/media/pawprint.5eceee15.png"
+  },
+  {
+    "revision": "021d5b82f4147c84a79a95f6fb76d684",
+    "url": "/AgenKlik/static/media/sedan.021d5b82.png"
+  }
+]);
